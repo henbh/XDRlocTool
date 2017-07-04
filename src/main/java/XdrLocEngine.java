66082f35064200c5888b20ed459bde00eda51a2c
henb <henbh7@gmail.com>
2017-07-04 10:16:37 +0300

    d

--- src/main/java/XdrLocEngine.java
+++ src/main/java/XdrLocEngine.java
@@ -1,6 +1,7 @@
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import config.ConfigurationManager;
+import javafx.util.Pair;
 import org.apache.commons.io.FileUtils;
 import org.json.JSONObject;
 
@@ -12,9 +13,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.HashMap;
 
 public class XdrLocEngine {
     private File mDoneFolder;
+    private HashMap mLocationHash = new HashMap();
 
     public static void main(String[] args) throws IOException {
         XdrLocEngine xdrLocEngine = new XdrLocEngine();
@@ -91,7 +94,7 @@ public class XdrLocEngine {
             String[] drs = item.split(",");
             try {
                 HttpResponse<String> response = Unirest.post("https://us1.unwiredlabs.com/v2/process.php")
-                        .body("{\"token\": \"95c59fe063cbaa\",\"radio\": \"gsm\",\"mcc\": " + drs[4] + ",\"mnc\": " + drs[5] + ",\"cells\": [{\"lac\": " + drs[3] + ",\"cid\": " + drs[2] + "}],\"address\": 1}")
+                        .body("{\"token\": \"95c59fe063cbaa\",\"radio\": \"gsm\",\"mcc\": " + drs[3] + ",\"mnc\": " + drs[4] + ",\"cells\": [{\"lac\": " + drs[2] + ",\"cid\": " + drs[1] + "}],\"address\": 1}")
                         .asString();
                 System.out.println(response.getBody().toString());
                 result = insertDB(response.getBody(), drs);
@@ -115,22 +118,22 @@ public class XdrLocEngine {
                             "msisdn," +
                             " imei," +
                             " imsi," +
-                            " msisdn," +
                             " latitude," +
                             " longitude," +
                             " mnc," +
                             " mcc " +
-                            ") VALUES (?,?,?,?,?,?,?,?,?,?)");
-        queryStatement.setLong(1, 0);
-        queryStatement.setLong(2,Integer.parseInt(data[2]));
-        queryStatement.setLong(3, 123123);
-        queryStatement.setLong(4, 2525656);
-        queryStatement.setLong(5, 123123);
-        queryStatement.setLong(6, 123123);
-        queryStatement.setFloat(7, Float.parseFloat(jsonObj.get("lat").toString()));
-        queryStatement.setFloat(8, Float.parseFloat(jsonObj.get("lon").toString()));
-        queryStatement.setLong(9, Integer.parseInt(data[4]));
-        queryStatement.setLong(10, Integer.parseInt(data[5]));
+                            ") VALUES (?,?,?,?,?,?,?,?,?)");
+        queryStatement.setLong(1,Long.parseLong(data[0]));
+        queryStatement.setLong(2,Long.parseLong(data[1]));
+        queryStatement.setLong(3, Long.parseLong(data[7]));
+        queryStatement.setLong(4, Long.parseLong(data[5]));
+        queryStatement.setLong(5, Long.parseLong(data[6]));
+        queryStatement.setFloat(6, Float.parseFloat(jsonObj.get("lat").toString()));
+        queryStatement.setFloat(7, Float.parseFloat(jsonObj.get("lon").toString()));
+        queryStatement.setLong(8, Integer.parseInt(data[4]));
+        queryStatement.setLong(9, Integer.parseInt(data[3]));
+
+//        mLocationHash.put(drs[2], new Pair<String,String>())
 
         queryStatement.execute();
         return result;
